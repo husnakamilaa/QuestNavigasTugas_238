d603ceb895dbd0673be699e3d931afdc5708e5a9
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-07 19:11:29 +0700

    feat: deklarasi TopBar Scaffold

--- app/src/main/java/com/example/navigasitugas/view/ListData.kt
+++ app/src/main/java/com/example/navigasitugas/view/ListData.kt
@@ -1,7 +1,15 @@
 package com.example.navigasitugas.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.navigasitugas.R
 
@@ -17,4 +25,11 @@ fun ListData(
         Pair(stringResource(R.string.status), "Lajang"),
         Pair(stringResource(R.string.alamat), "Sleman")
     )
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(R.string.list), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(R.color.purple_200))
+            )
+        }) {  }
 }
