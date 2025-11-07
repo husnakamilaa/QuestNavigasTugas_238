da2d92eca6da2e2b4d5be1b144c9594b906987a9
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-07 18:21:47 +0700

    feat: mendeklarasikan layout utama column pada body scaffold

--- app/src/main/java/com/example/navigasitugas/view/Beranda.kt
+++ app/src/main/java/com/example/navigasitugas/view/Beranda.kt
@@ -1,2 +1,29 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.navigasitugas.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.navigasitugas.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Beranda(
+    OnListDataBtnClick: () -> Unit
+){
+    Scaffold { isiRuang->
+        val gambar = painterResource(id = R.drawable.pastry)
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally){
+
+        }
+    }
+}
